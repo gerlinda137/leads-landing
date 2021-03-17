b33b61acd070e09f1c4a953909888fab817c2d60
gerlinda137 <gerlinda137@gmail.com>
2021-03-18 02:00:07 +0300

    styling in progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,33 @@
-import { Swiper, EffectFade } from 'swiper';
+import { Swiper, EffectFade, Pagination } from 'swiper';
 import 'swiper/swiper-bundle.css';
 
 import 'normalize.css';
-Swiper.use([EffectFade]);
+Swiper.use([EffectFade, Pagination]);
 
-const bgSlider = document.querySelector(`.swiper-container`);
 
+// const bgSlider = document.querySelector(`.swiper-container`);
+
+// // eslint-disable-next-line no-unused-vars
+// const mySwiper = new Swiper(bgSlider, {
+//   slidesPerView: 1,
+//   speed: 1000,
+//   effect: `fade`,
+//   fadeEffect: {
+//     crossFade: true
+//   },
+// });
+
+const slider = document.querySelector(`.swiper-container`);
 // eslint-disable-next-line no-unused-vars
-const mySwiper = new Swiper(bgSlider, {
+const swiper = new Swiper(slider, {
   slidesPerView: 1,
-  speed: 1000,
+  speed: 2000,
   effect: `fade`,
   fadeEffect: {
     crossFade: true
   },
+  pagination: {
+    el: `.swiper-pagination`,
+    clickable: true,
+  },
 });
-
