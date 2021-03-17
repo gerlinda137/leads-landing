69a801142691de1e47962984ba936f66e43c3c85
gerlinda137 <gerlinda137@gmail.com>
2021-03-17 22:41:27 +0300

    finished header

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,18 @@
-import Swiper from 'swiper';
+import { Swiper, EffectFade } from 'swiper';
 import 'swiper/swiper-bundle.css';
 
 import 'normalize.css';
+Swiper.use([EffectFade]);
 
-const gbgbug = new Swiper();
-gbgbug.nygbugb();
+const bgSlider = document.querySelector(`.swiper-container`);
+
+// eslint-disable-next-line no-unused-vars
+const mySwiper = new Swiper(bgSlider, {
+  slidesPerView: 1,
+  speed: 1000,
+  effect: `fade`,
+  fadeEffect: {
+    crossFade: true
+  },
+});
 
